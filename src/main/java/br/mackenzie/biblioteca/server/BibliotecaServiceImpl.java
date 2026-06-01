f2d2d5b9b29376de78b965afd4af46fd62629557
NewsMoney <milton.almeida@live.com>
2026-06-01 12:28:39 -0300

    Create BibliotecaServiceImpl.java

---
+++ src/main/java/br/mackenzie/biblioteca/server/BibliotecaServiceImpl.java
@@ -0,0 +1,54 @@
+package br.mackenzie.biblioteca.model;
+
+public class Livro {
+
+    private int id;
+    private String titulo;
+    private String autor;
+    private int ano;
+    private String isbn;
+
+    public Livro(int id,
+                 String titulo,
+                 String autor,
+                 int ano,
+                 String isbn) {
+
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.isbn = isbn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", ano=" + ano +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
+}
